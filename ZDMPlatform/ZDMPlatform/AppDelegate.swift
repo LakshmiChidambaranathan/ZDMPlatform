0ccff972f7aa7de5e3469351145bab5416a35cc8
Lakshmi Chidambaranathan <lakshmi.cg@zohocorp.com>
2023-09-27 18:48:50 +0530

    --> Zoholics demo app setup

---
+++ ZDMPlatform/ZDMPlatform/AppDelegate.swift
@@ -0,0 +1,120 @@
+//
+//  AppDelegate.swift
+//  ZDMPlatform
+//
+//  Created by lakshmi-12493 on 22/09/23.
+//
+
+import UIKit
+import CoreData
+import ZohoDeskPortalAPIKit
+import ZohoDeskPortalConfiguration
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        ZDMASAP.setupSDK(with: .deskMobileNew)
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+        */
+        let container = NSPersistentContainer(name: "ZDMDemoApp")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                 
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+}
+
+enum ASAPConfiguration {
+    case singleDept, deskMobile, corp, portalSDK, gcPlatformiOS, deskMobileNew, portalSDKNew
+}
+
+final class ZDMASAP {
+    
+    struct Credentials {
+        var appId: String
+        var orgId: String
+        var bundleId: String
+    }
+    
+    class func setupSDK(with config : ASAPConfiguration) {
+        var credentials : Credentials
+        switch config {
+        case .singleDept:
+            credentials = Credentials(appId: "edbsn4d60871cfc4c4a12f9cf3012b35217211795d8154c34fd723e34abcb65fccc19", orgId: "648638721", bundleId: "deskexmaple.zoho.com.deskexampleapp")
+        case .deskMobile:
+            credentials = Credentials(appId: "b10c3c638547955c73abf2fc9f38cc75f57d9b02e22471ff", orgId: "648638721", bundleId: "com.portalDemo.app")
+        case .corp:
+            credentials = Credentials(appId: "edbsne2cad5875cf6860518b4caabe63e9543", orgId: "4241905", bundleId: "com.desk.sdkdemo.zohocorp")
+        case .portalSDK:
+            credentials = Credentials(appId: "edbsna4c72ecf4eb28b468bf19578a6457af019184dcf5f57b8992cc84599ef0992b3", orgId: "695259828", bundleId: "com.portalDemo.app")
+        case .gcPlatformiOS :
+            credentials = Credentials(appId: "edbsn3e634a3215319385dfde58ab8ab9c87a2b350c4af59f4171efbee708ed84a9fe", orgId: "695259828", bundleId: "com.demo.gc.platform")
+        case .deskMobileNew:
+            credentials = Credentials(appId: "edbsn8feff784431f96819974766bcb79c45e37ddfe79550da844c1dc28672f9d5d79", orgId: "648638721", bundleId: "com.zoho.desk.asapsdk")
+        case .portalSDKNew :
+            credentials = Credentials(appId: "edbsnc25778b2ef6741d78f349474f93d13008608c17e3574b0e604269e24e502040f", orgId: "695259828", bundleId: "com.zoho.desk.asapsdk")
+        }
+        if !credentials.bundleId.isEmpty {
+            PNConstants.bundleName = credentials.bundleId
+        }
+        ZohoDeskPortalKit.initialize(orgID: credentials.orgId, appID: credentials.appId)
+    }
+}
---
+++ ZDMPlatform/ZDMPlatform/ViewController.swift
@@ -0,0 +1,327 @@
+//
+//  ViewController.swift
+//  ZDMPlatform
+//
+//  Created by lakshmi-12493 on 22/09/23.
+//
+
+import UIKit
+import ZohoDeskPortalAPIKit
+import ZohoDeskPortalTicket
+import ZPComponents
+import ZohoDeskPlatformUIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var launchButton : UIButton!
+    @IBOutlet var customizeButton : UIMenuButton!
+    @IBOutlet var infoLabel : UILabel!
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    @IBAction func logoutTapped(_ sender: UIButton) {
+        self.showLogoutAlert()
+    }
+    @IBAction func launchScreen(_ sender: UIButton) {
+        switch sender.titleLabel?.text {
+        case loginText : signInTapped(sender)
+        case accessTicket :
+            if !ZohoDeskPortalKit.isUserLoggedIn { self.showAlert(with: "Kindly Login to Access your Tickets!"); return }
+            ZDPortalTicket.show(style: style)
+        default : break
+        }
+    }
+    
+    @IBAction func launchComponents(_ sender: UIButton) {
+        ZDPBuilderSDK.init(initial: "ZPHome", includeBinder: ZPComponentsProvider()) { (controller) in
+             DispatchQueue.main.async {
+                self.navigationController?.pushViewController(controller, animated: true)
+             }
+        }
+    }
+    
+    lazy var blurView : UIView = {
+        let bgView = UIView()
+        bgView.backgroundColor = .systemGray6.withAlphaComponent(0.5)
+        self.launchButton.addSubview(bgView)
+        bgView.g_pinEdges()
+        bgView.isHidden = true
+        return bgView
+    }()
+    lazy var loader : UIActivityIndicatorView = {
+        let loader = UIActivityIndicatorView()
+        loader.tintColor = .white
+        self.blurView.addSubview(loader)
+        loader.g_pinEdges()
+        loader.isHidden = true
+        return loader
+    }()
+    
+    let loginText : String = "Login"
+    let accessTicket : String = "Access Tickets"
+    lazy var signInProfile : ZDPortal.Profile? = nil
+    var style : Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.initialSetUp()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    func initialSetUp() {
+        self.infoLabel.text = "Users can now customize their ticket view!"
+        self.logoutButton.isHidden = true
+        self.customizeButton.setup(values: ["Default Style", "Custom Style 1", "Custom Style 2"], delegate: self)
+        self.launchButton.setTitle(ZohoDeskPortalKit.isUserLoggedIn ? "Signing in...": loginText, for: .normal)
+        ZohoDeskPortalKit.syncSettings { result in
+            self.updateSignInValue()
+        }
+    }
+    
+    func updateSignInValue() {
+        ZohoDeskPortalKit.getUserInformation { result in
+            DispatchQueue.main.async {
+                self.stop()
+                switch result {
+                case .success(let profile):
+                    self.launchButton.setTitle(self.accessTicket, for: .normal)
+                    self.signInProfile = profile
+                    self.logoutButton.isHidden = false
+                case .failure(_):
+                    self.launchButton.setTitle(self.loginText, for: .normal)
+                }
+            }
+        }
+    }
+    
+    @objc private func signInTapped(_ sender: UIButton) {
+        UISelectionFeedbackGenerator().selectionChanged()
+        if !ZohoDeskPortalKit.isUserLoggedIn {
+            sender.setTitle("Signing in...", for: .normal)
+            let loginAlert = UIAlertController(title: "Login Using Email Id", message: nil, preferredStyle: .alert)
+            loginAlert.view.tintColor = .systemBlue
+            var emailTextField: UITextField? = nil
+            loginAlert.addTextField { textField in
+                emailTextField = textField
+                textField.font = .systemFont(ofSize: 17.0)
+                textField.placeholder = "Enter your email id"
+                textField.text = /*"deskqa17@gmail.com"*/"vignesh.thillai@zohocorp.com"
+            }
+            let cancelAction = UIAlertAction(title: "Cancel",
+                                             style: .destructive,
+                                             handler: { _ in
+                self.updateSignInValue()
+            })
+            loginAlert.addAction(cancelAction)
+            
+            let loginAction = UIAlertAction(title: "Login",
+                                            style: .default,
+                                            handler: { _ in
+                guard let textField = emailTextField, let email = textField.text, email.isValidEmail else {
+                    let message = "Enter a valid email id!"
+                    loginAlert.setValue(NSMutableAttributedString(string: message, attributes: [NSAttributedString.Key.foregroundColor : UIColor.red, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14)]), forKey: "attributedMessage")
+                    
+                    self.present(loginAlert, animated: true, completion: nil)
+                    return
+                }
+                self.start()
+                ZohoDeskPortalKit.login(withUserToken: email) { (isSucceed) in
+                    self.updateSignInValue()
+                }
+            })
+            
+            loginAlert.addAction(loginAction)
+            loginAlert.preferredAction = loginAction
+            present(loginAlert, animated: true, completion: nil)
+        }
+    }
+    
+    private func loginASAP(with email: String = /*"deskqa17@gmail.com"*/"vignesh.thillai@zohocorp.com") {
+        ZohoDeskPortalKit.login(withUserToken: email) { (isSucceed) in
+            if !(isSucceed) {
+                self.showAlert(with: "Login failed")
+            }
+        }
+    }
+    
+    private func showAlert(with message: String) {
+        DispatchQueue.main.async {
+            let alertController = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+            let dismissAction = UIAlertAction(title: "Dismiss", style: .destructive) { (_) in
+                alertController.dismiss(animated: true, completion: nil)
+            }
+            alertController.addAction(dismissAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    private func showLogoutAlert() {
+        DispatchQueue.main.async {
+            let alertController = UIAlertController(title: "Logout", message: "Are you sure to logout \(self.signInProfile?.emailID)?", preferredStyle: .alert)
+            let dismissAction = UIAlertAction(title: "No", style: .cancel) { (_) in
+                alertController.dismiss(animated: true, completion: nil)
+            }
+            alertController.addAction(dismissAction)
+            let logoutAction = UIAlertAction(title: "Yes", style: .destructive) { _ in
+                self.start()
+                ZohoDeskPortalKit.logout { (isSucceed) in
+                    DispatchQueue.main.async {
+                        self.logoutButton.isHidden = true
+                        self.updateSignInValue()
+                    }
+                }
+            }
+            alertController.addAction(logoutAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    func start() {
+        self.blurView.isHidden = false
+        self.loader.isHidden = false
+        self.loader.startAnimating()
+    }
+    func stop() {
+        self.loader.stopAnimating()
+        self.loader.isHidden = true
+        self.blurView.isHidden = true
+    }
+    
+}
+
+extension ViewController : UIMenuButtonActionable {
+    
+    func didSelect(index: Int, title: String) {
+        self.style = index
+        self.start()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.stop()
+        }
+        
+        self.infoLabel.alpha = .zero
+        
+        switch self.style {
+        case 1, 2: self.infoLabel.text = "Ticket view is now customized with \(title)"
+        default : self.infoLabel.text = "Users can now customize their ticket view!"
+        }
+        
+        UIView.animate(withDuration: 0.75) {
+            self.infoLabel.alpha = 1
+        }
+    }
+}
+
+extension UIView {
+    func g_pinCenter(view: UIView? = nil) {
+        g_pin(on: .centerX, view: view)
+        g_pin(on: .centerY, view: view)
+    }
+    
+    @discardableResult internal func g_pinEdges(view: UIView? = nil, priority: Float? = nil) -> (left: NSLayoutConstraint?, right: NSLayoutConstraint?, top: NSLayoutConstraint?, bottom: NSLayoutConstraint?) {
+        let top = g_pin(on: .top, view: view, priority: priority)
+        let bottom = g_pin(on: .bottom, view: view, priority: priority)
+        let left = g_pin(on: .left, view: view, priority: priority)
+        let right = g_pin(on: .right, view: view, priority: priority)
+        return (left, right, top, bottom)
+    }
+    @discardableResult internal func g_pin(on type1: NSLayoutConstraint.Attribute,
+                                           view: UIView? = nil,
+                                           on type2: NSLayoutConstraint.Attribute? = nil,
+                                           constant: CGFloat = 0,
+                                           relatedBy relation: NSLayoutConstraint.Relation = .equal,
+                                           priority: Float? = nil) -> NSLayoutConstraint? {
+        guard let view = view ?? superview else {
+            return nil
+        }
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        let type2 = type2 ?? type1
+        let constraint = NSLayoutConstraint(item: self, attribute: type1,
+                                            relatedBy: relation,
+                                            toItem: view, attribute: type2,
+                                            multiplier: 1, constant: constant)
+        if let priority = priority {
+            constraint.priority = UILayoutPriority.init(priority)
+        } else {
+            constraint.priority = UILayoutPriority.init(999)
+        }
+        
+        constraint.isActive = true
+        
+        return constraint
+    }
+}
+
+
+
+
+protocol UIMenuButtonActionable: AnyObject {
+    
+    func didSelect(index: Int, title: String)
+}
+
+class UIMenuButton: UIButton {
+    
+    private var actions: [String] = []
+    private weak var delegate: UIMenuButtonActionable?
+    
+    func setup(values: [String], delegate: UIMenuButtonActionable?) {
+        
+        self.delegate = delegate
+        self.actions = values
+        self.prepare()
+    }
+    
+    func prepare() {
+        if #available(iOS 14.0, *) {
+            let actions = actions.map({
+                let action = UIAction(title: $0) { [weak self] action in
+                    guard let self else { return }
+                    if let index = self.actions.firstIndex(of: action.title) {
+                        self.delegate?.didSelect(index: index, title: action.title)
+                    }
+                }
+                return action
+            })
+            
+            let menu = UIMenu(title: "", children: actions)
+            self.showsMenuAsPrimaryAction = true
+            self.menu = menu
+        }
+        
+        self.addTarget(self, action: #selector(onTap), for: .touchUpInside)
+    }
+    
+    @objc
+    private func onTap() {
+        let actionsheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        actions.forEach({
+            actionsheet.addAction(
+                UIAlertAction(title: $0, style: .default) { [weak self] action in
+                    guard let self else { return }
+                    if let title = action.title, let index = self.actions.firstIndex(of: title) {
+                        self.delegate?.didSelect(index: index, title: title)
+                    }
+                })
+        })
+        actionsheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.getTopController()?.present(actionsheet, animated: true)
+    }
+    
+    private func getTopController() -> UIViewController? {
+        if var topController = UIApplication.shared.keyWindow?.rootViewController {
+            while let presentedViewController = topController.presentedViewController {
+                topController = presentedViewController
+            }
+            return topController
+        }
+        return nil
+    }
+}
+
+
